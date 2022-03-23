c5073faacbcf5afa4912b629d251e40719dba912
Faiq Malik <faiqmalik2@gmail.com>
2022-03-23 01:01:21 -0400

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   }
   return (
     <div className="App">
-      <Button onClick={handleVisibility}>TOGGLE</Button>
+      <Button className="toggle-btn" onClick={handleVisibility}>TOGGLE</Button>
       {isVisible && <CustomTab />}
     </div>
   );
